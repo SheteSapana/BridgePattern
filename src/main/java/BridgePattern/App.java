a8c29de7cefa47b5733345f922c19dcc5f888c6d
SheteSapana <shetesapana@gmail.com>
2021-07-05 16:24:43 -0700

    test

---
+++ src/main/java/BridgePattern/App.java
@@ -0,0 +1,13 @@
+package BridgePattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
